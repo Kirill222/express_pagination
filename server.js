9e0fb7f7d04d2ce1b65288b543f80bf39d85f96b
John Connor <zkmd2012@inbox.ru>
2021-09-25 13:28:51 +0500

    01. Basic pagination logic

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+const users = [
+    {id: 1, name: "user 1"},
+    {id: 2, name: "user 2"},
+    {id: 3, name: "user 3"},
+    {id: 4, name: "user 4"},
+    {id: 5, name: "user 5"},
+    {id: 6, name: "user 6"},
+    {id: 7, name: "user 7"},
+    {id: 8, name: "user 8"},
+    {id: 9, name: "user 9"},
+    {id: 10, name: "user 10"},
+    {id: 11, name: "user 11"},
+    {id: 12, name: "user 12"},
+    {id: 13, name: "user 13"},
+    {id: 14, name: "user 14"},
+]
+
+app.get('/users', (req, res, next) => {
+    const page = req.query.page
+    const limit = req.query.limit
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const result = users.slice(startIndex, endIndex)
+
+    res.json(result)
+})
+
+app.listen(5000, () => console.log("Run 5000"))
