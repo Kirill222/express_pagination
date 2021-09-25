21fe2c7c399fba405c5276cabdb0bf33a0700ccc
John Connor <zkmd2012@inbox.ru>
2021-09-25 14:07:35 +0500

    03. Middleware with encapsulated pagination logic is created

--- server.js
+++ server.js
@@ -19,33 +19,63 @@ const users = [
     {id: 14, name: "user 14"},
 ]
 
-app.get('/users', (req, res, next) => {
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+const posts = [
+    {id: 1, name: "post 1"},
+    {id: 2, name: "post 2"},
+    {id: 3, name: "post 3"},
+    {id: 4, name: "post 4"},
+    {id: 5, name: "post 5"},
+    {id: 6, name: "post 6"},
+    {id: 7, name: "post 7"},
+    {id: 8, name: "post 8"},
+    {id: 9, name: "post 9"},
+    {id: 10, name: "post 10"},
+    {id: 11, name: "post 11"},
+    {id: 12, name: "post 12"},
+    {id: 13, name: "post 13"},
+    {id: 14, name: "post 14"},
+]
 
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
-
-    const result = {}
-
-    if (endIndex < users.length) {
-        result.next = {
-            page: page + 1,
-            limit: limit
-        }  
-    }   
-
-    if (startIndex > 0) {
-        result.prev = {
-            page: page - 1,
-            limit: limit
-        }
-    }
-    
-
-    result.results = users.slice(startIndex, endIndex)
-
-    res.json(result)
+app.get('/posts', paginatedResults(posts), (req, res, next) => {
+    res.json(res.paginatedResults)
 })
+
+
+app.get('/users', paginatedResults(users), (req, res, next) => {
+    res.json(res.paginatedResults)
+})
+
+function paginatedResults(data) {
+    return (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const result = {}
+
+        if (endIndex < data.length) {
+            result.next = {
+                page: page + 1,
+                limit: limit
+            }  
+        }   
+
+        if (startIndex > 0) {
+            result.prev = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+        
+
+        result.results = data.slice(startIndex, endIndex)
+
+        res.paginatedResults = result
+
+        next()
+        }
+}
 
 app.listen(5000, () => console.log("Run 5000"))
